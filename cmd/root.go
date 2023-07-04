77f976bf518c06dfa9709472e45247ac1c4808d5
Moritz Gottschling <moritz@giantswarm.io>
2023-07-04 09:20:09 +0200

    Add version command

--- cmd/root.go
+++ cmd/root.go
@@ -3,22 +3,15 @@ package cmd
 import (
 	"os"
 
+	"github.com/giantswarm/nancy-fixer/pkg/project"
 	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "nancy-fixer",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Nancy fixer is a tool to fix nancy vulnerabilities",
+	Version: project.Version(),
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
---
+++ pkg/project/project.go
@@ -0,0 +1,29 @@
+package project
+
+var (
+	description = "CLI to automatically fix nancy vulnerabilities"
+	gitSHA      = "n/a"
+	name        = "nancy-fixer"
+	source      = "https://github.com/giantswarm/nancy-fixer"
+	version     = "0.1.0-dev"
+)
+
+func Description() string {
+	return description
+}
+
+func GitSHA() string {
+	return gitSHA
+}
+
+func Name() string {
+	return name
+}
+
+func Source() string {
+	return source
+}
+
+func Version() string {
+	return version
+}
