ef18dc6c0c190594a558e6ba82d3a14be0642f6b
taylorbot <dev@giantswarm.io>
2025-05-28 09:12:26 +0000

    Release v0.5.1

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "nancy-fixer"
 	source      = "https://github.com/giantswarm/nancy-fixer"
-	version     = "0.5.1-dev"
+	version     = "0.5.1"
 )
 
 func Description() string {
