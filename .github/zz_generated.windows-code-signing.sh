4a1f625aeb76a8ffe5a04f3622dc2d127f57afb1
ArchitectBot <61872893+architectbot@users.noreply.github.com>
2026-04-15 19:42:16 +0200

    Align files (#197)
    Co-authored-by: github-actions <action@github.com>

--- .github/zz_generated.windows-code-signing.sh
+++ .github/zz_generated.windows-code-signing.sh
@@ -4,7 +4,7 @@
 #
 #    devctl
 #
-#    https://github.com/giantswarm/devctl/blob/d0c8f03c86ad4cfaef54d2315dc6f6d402be716d/pkg/gen/input/makefile/internal/file/windows-code-signing.sh.template
+#    https://github.com/giantswarm/devctl/blob/c2dd604fd787d9aa63ec6c43c817c8596f1356f7/pkg/gen/input/makefile/internal/file/windows-code-signing.sh.template
 #
 
 APPLICATION=$1
